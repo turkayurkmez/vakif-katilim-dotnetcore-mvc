b129c8b176f4c31f06105cbd1b72dc82bd74685d
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-04 12:32:00 +0300

    introducing dotnet core & mvc

---
+++ introduceDotNetCore/introduceDotNetCore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace introduceDotNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int? id = null)
+        {
+            ViewBag.Country = "Türkiye";
+            ViewBag.Day = DateTime.Now.Day;
+            return View();
+
+
+        }
+    }
+}
--- introduceDotNetCore/introduceDotNetCore/Program.cs
+++ introduceDotNetCore/introduceDotNetCore/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var value = builder.Configuration.GetValue<string>("Value");
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.UseWelcomePage();
+
+
+app.Logger.LogInformation(value);
+
+
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
